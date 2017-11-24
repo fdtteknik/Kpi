5e685548fb5ec87d6611cd99bdc5fc0368c970ee
U-FDT\MikaelE <MikaelE@DESKTOP-D22J9SK.FDT.AD>
2017-11-24 09:43:29 +0100

    Två .html har lagts till och bat scriptet har uppdaterats för att flytta dessa till websiiten

--- KpiService.bat
+++ KpiService.bat
@@ -2,3 +2,12 @@ mkdir C:\KPI
 cd C:\KPI
 git pull
 python C:\Kpi\KpiCollector.py
+mkdir c:\inetpub
+mkdir c:\inetpub\wwwroot
+copy RotatingDisplay.html c:\inetpub\wwwroot\.
+copy KPI.html c:\inetpub\wwwroot\.
+copy SexKPIer.html c:\inetpub\wwwroot\.
+copy KPI-URLs.csv c:\inetpub\wwwroot\.
+copy *.js c:\inetpub\wwwroot\.
+
+
