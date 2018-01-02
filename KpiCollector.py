e6d481c7ea4020cb8ecb24a23152eb549ffec4cc
fdtteknik <teknik@fdt.se>
2018-01-02 11:38:20 +0100

    Sigh

--- KpiCollector.py
+++ KpiCollector.py
@@ -10,10 +10,10 @@ from dateutil.relativedelta import *
 logger = logging.getLogger(os.path.basename(sys.argv[0]))
 logger.setLevel(logging.DEBUG)
 
-#_path = "C:\\KPI\\"
+_path = "C:\\KPI\\"
 #_path = "http://10.124.0.81/"
 #_path = "http://10.124.0.81/"
-_path = "C:\\inetpub\\wwwroot\"
+#_path = "C:\\inetpub\\wwwroot\"
 
 # KStalle mapping
 # SELECT TOP (1000) * FROM [boka100].[dbo].[Kställe]
